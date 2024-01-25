002813cb8e3dbf4c50ab7169c323c3e0a59470d3
DavidXploidz <david.adame.itd@gmail.com>
2024-01-25 16:37:56 -0600

    kegen

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/online_store/'
+  publicPath: '/online_store/',
+  transpileDependencies: true
 })
